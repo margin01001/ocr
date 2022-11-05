0e815d54b24c1023d583f6fcbdc27bdfb25280c9
margin01001 <margin01001@gmail.com>
2022-11-05 16:42:08 +0530

    async

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, request, render_template, flash, redirect
 from ocr import extractInfo
 import os
 from werkzeug.utils import secure_filename
+import threading
+import asyncio
 
+print(f"In flask global level: {threading.current_thread().name}")
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = 'uploads/images'
@@ -17,8 +20,13 @@ def allowedFile(filename):
 def home():
     return render_template('home.html')
 
+async def hello():
+    await asyncio.sleep(1)
+    return 1
+
 @app.route('/', methods=['POST'])
 def upload():
+    print(f"In flask global level: {threading.current_thread().name}")
     file = request.files['file']
     
     if file.filename == '':
@@ -27,7 +35,9 @@ def upload():
     elif file :#and allowedFile(file.filename):
         file_dir = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
         file.save(file_dir)
-
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        loop = asyncio.get_event_loop()
+        result = loop.run_until_complete(hello())
         info = extractInfo(file_dir)
         name = ', '.join(info.extract_name())
         email = ', '.join(info.extract_email())
